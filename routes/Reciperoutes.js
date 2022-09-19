8aa9dcbda4bbb7d8040f847474e7390506fca8d4
Nikhil <nikhilshetye24@gmail.com>
2022-09-19 15:51:05 -0700

    added create recipe functionality

--- routes/Reciperoutes.js
+++ routes/Reciperoutes.js
@@ -2,9 +2,22 @@ const express = require('express');
 const router = express.Router();
 const {getAllRecipes,createRecipe,getRecipesById,updateRecipe,deleteRecipe} = require('../controllers/recipeControllers')
 
+const multer  = require('multer');
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname)
+  }
+});
+
+const upload = multer({ storage: storage })
+
 router.route('/')
             .get(getAllRecipes)
-            .post(createRecipe)
+            .post(createRecipe,upload.single('fileImg'))
 
 router.route('/:id')
             .get(getRecipesById)
