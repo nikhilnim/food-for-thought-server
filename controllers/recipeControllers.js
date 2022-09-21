3d60b8d1d644db902f0487223bbc9bf8e0459ced
Nikhil <nikhilshetye24@gmail.com>
2022-09-20 23:15:49 -0700

    edit recipe route fine tuned to accept images

--- controllers/recipeControllers.js
+++ controllers/recipeControllers.js
@@ -12,7 +12,19 @@ function getAllRecipes(req, res) {
   });
 }
 
-function getRecipesById(req, res) {}
+function getRecipesById(req, res) {
+  loadRecipeData((err,data)=>{
+    if(err){
+      res.status(500).send('File Read err')
+    }else{
+      let allRecipes = JSON.parse(data)
+      let foundRecipe = allRecipes.find((e)=>{
+        return e.id === req.params.id
+      })
+      res.json(foundRecipe)
+    }
+  })
+}
 
 function createRecipe(req, res) {
   const { body: recipe } = req;
@@ -38,7 +50,7 @@ function createRecipe(req, res) {
     id:uuidv4(),
     title:recipe.title,
     type:recipe.type,
-    image:req.file.imageName,
+    image:req.file ? req.file.imageName  : 'placeimg_633_411_tech.jpg',
     cookTime:recipe.cookTime,
     direction:recipe.direction,
     nutrition:{
@@ -74,7 +86,67 @@ function createRecipe(req, res) {
 
 }
 
-function updateRecipe(req, res) {}
+function updateRecipe(req, res) {
+  const { body: recipe } = req;
+  if (
+    !recipe.title ||
+    !recipe.intro ||
+    !recipe.type ||
+    !recipe.prepTime ||
+    !recipe.cookTime ||
+    !recipe.serving ||
+    !recipe.ingredient ||
+    !recipe.calories ||
+    !recipe.carbs ||
+    !recipe.protein ||
+    !recipe.fat || 
+    !recipe.direction   
+  ) {
+    res.status(206).send('Partial Content')
+    return
+  }
+
+  let updatedRecipe = {
+    id:req.params.id,
+    title:recipe.title,
+    type:recipe.type,
+    image:req.file ? req.file.imageName  : recipe.ogiImageName,
+    cookTime:recipe.cookTime,
+    direction:recipe.direction,
+    nutrition:{
+      calories:recipe.calories,
+      fat:recipe.fat,
+      protein:recipe.protein,
+      carbs:recipe.carbs,
+    },
+    ingredient:recipe.ingredient,
+    intro:recipe.intro,
+    prepTime:recipe.prepTime,
+    serving:recipe.serving,
+    rating:[{
+      rating: 4.5,
+      memberId:1,
+      comment:"added zucchinni and summer squash and really like it with more vegetables",
+      timeStamp:1519211809934
+    }],
+    timeStamp:Date.now()
+  }
+  console.log(updatedRecipe)
+  loadRecipeData((err, data)=>{
+    if(err){
+      res.send("error reading file")
+    }else{
+      let allRecipes = JSON.parse(data)
+      let recipeIndex = allRecipes.findIndex((e)=>{
+          return e.id === req.params.id
+      })
+      allRecipes.splice(recipeIndex,1,updatedRecipe)
+      console.log(allRecipes)
+      saveRecipeData(JSON.stringify(allRecipes))
+      res.json(updatedRecipe)
+    }
+  })
+}
 
 function deleteRecipe(req, res) {}
 
