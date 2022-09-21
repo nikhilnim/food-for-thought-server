ed1e666a8b59580df115f0ce871a9cc3bbfc5238
Nikhil <nikhilshetye24@gmail.com>
2022-09-21 14:51:22 -0700

    image added by api

--- routes/Reciperoutes.js
+++ routes/Reciperoutes.js
@@ -9,7 +9,6 @@ const storage = multer.diskStorage({
     cb(null, 'public/images/')
   },
   filename: function (req, file, cb) {
-    // cb(null, file.originalname)
     console.log("file",file)
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
     const filname = `${uniqueSuffix}${file.originalname}`
